0e0efd297526b327b15c9b8080ca6c9b2737a281
nikolimaj <56877942+nikolimaj@users.noreply.github.com>
2023-06-13 12:16:43 +0200

    Revert "Sara gene graph"

--- cypress/component/component-health-check.cy.tsx
+++ cypress/component/component-health-check.cy.tsx
@@ -1,5 +1,4 @@
 import * as React from 'react';
-import { mount } from 'cypress/react';
 import { VisynApp, VisynAppProvider } from 'visyn_core/app';
 
 describe('Health check for Cypress component test', () => {
